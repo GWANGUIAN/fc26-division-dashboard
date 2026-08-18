bfef8e019fc9e3a12e4984197cfb1f74760c93e0
GWANGUIAN <85675881+GWANGUIAN@users.noreply.github.com>
2026-08-18 15:18:32 +0900

    feat: sort music playlist

--- src/web/musicPlaylist.ts
+++ src/web/musicPlaylist.ts
@@ -1,11 +1,11 @@
 export type MusicTrack = { videoId: string; title: string; artist: string };
 
 export const musicPlaylist: readonly MusicTrack[] = [
+  { videoId: "ilgEJDnsUss", title: "We are DINOS - 버축대 천양 다이노스 / cover 하디아", artist: "하디아" },
+  { videoId: "aYBdAjG02Zg", title: "우왁굳 - Ballin (왁컬로이드 COVER) [버축대 두세븐일레븐 MV]", artist: "진코2" },
   { videoId: "79rDMDll-IY", title: "Ballin - 버축대 두세븐일레븐 MV", artist: "왁타버스 WAKTAVERSE" },
   { videoId: "3ugetzmbQF8", title: "We are DINOS - 버축대 천양 다이노스 MV", artist: "왁타버스 WAKTAVERSE" },
   { videoId: "dnK6P5rI2Dg", title: "티바로살래나 후보 MV - 버축대 3팀", artist: "왁타버스 WAKTAVERSE" },
   { videoId: "VCV6aVlyyS4", title: "티바로살래나 주전 MV - 버축대 3팀", artist: "왁타버스 WAKTAVERSE" },
   { videoId: "4di6nanDjiU", title: "Counting Stars - 버축대 카오닝스타즈 MV", artist: "왁타버스 WAKTAVERSE" },
-  { videoId: "aYBdAjG02Zg", title: "우왁굳 - Ballin (왁컬로이드 COVER) [버축대 두세븐일레븐 MV]", artist: "진코2" },
-  { videoId: "ilgEJDnsUss", title: "We are DINOS - 버축대 천양 다이노스 / cover 하디아", artist: "하디아" },
 ];
