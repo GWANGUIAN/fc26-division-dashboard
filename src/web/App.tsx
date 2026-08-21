1c8aea3228781d17135926b6db5f0b516d974c91
GWANGUIAN <85675881+GWANGUIAN@users.noreply.github.com>
2026-08-21 19:42:50 +0900

    feat: change wording

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -177,7 +177,7 @@ const ANNOUNCEMENTS: Announcement[] = [
           <Users aria-hidden="true" />
           <span>나만의 스쿼드 빌더</span>
         </span>{" "}
-        버튼을 누르면 스쿼드를 추가하고 원하는 포메이션에 스트리머들을 자유롭게 배치해볼 수
+        버튼을 누르면 스쿼드를 추가하고 원하는 포메이션에 선수들을 자유롭게 배치해볼 수
         있습니다.
       </>
     ),
