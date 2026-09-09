7995b798d95aaebc776e6c5c161d9ac2bb34e36f
GWANGUIAN <85675881+GWANGUIAN@users.noreply.github.com>
2026-09-10 02:26:01 +0900

    feat: add ad

--- src/web/fakeAdsData.ts
+++ src/web/fakeAdsData.ts
@@ -61,4 +61,10 @@ export const fakeAds: FakeAd[] = [
     href: "https://www.sooplive.com/station/hachi97",
     label: "패러디 광고 - 하치",
   },
+    {
+    id: "yukira",
+    image: "/fake-ads/ad-yukira.webp",
+    href: "https://vod.sooplive.com/player/206434535/catch",
+    label: "패러디 광고 - 유키라",
+  },
 ];
